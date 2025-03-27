70da41bc6588bd14bacf08e3613338d3bbe675ce
Ruth Olasupo <rolasupo@norquest.ca>
2025-03-27 17:33:43 -0600

    Update app.py

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ def predictions_page():
     avg_days_between_pickups = st.number_input("Avg Days Between Pickups", min_value=1.0, max_value=100.0, step=0.1)
     #month = st.number_input("Month", min_value=1, max_value=12, step=1)
     days_since_last_pickup = st.number_input("Days Since Last Pickup", min_value=0, step=1)
-    year_month = st.selectbox("Year-Month", ["2024-08", "2024-07", "2024-06"])
+    #year_month = st.selectbox("Year-Month", ["2024-08", "2024-07", "2024-06"])
     
     # Prepare input data
     input_data = {
